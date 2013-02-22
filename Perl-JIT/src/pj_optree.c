a50cfb3ce19cff099cc32e6d1ea9c7ecf2a92b5a
Steffen Mueller <smueller@cpan.org>
2013-02-22 15:35:20 +0100

    Check in for WIP: tree walking and OP tree => AST stage

--- Perl-JIT/src/pj_optree.c
+++ Perl-JIT/src/pj_optree.c
@@ -5,6 +5,104 @@
 #include "ppport.h"
 #include "pj_debug.h"
 #include "stack.h"
+#include "pj_ast_terms.h"
+
+#define IS_JITTABLE_ROOT_OP_TYPE(otype) \
+        (otype == OP_ADD || otype == OP_SUBTRACT)
+
+#define IS_JITTABLE_OP_TYPE(otype) \
+        (IS_JITTABLE_ROOT_OP_TYPE(otype) \
+          || otype == OP_PADSV \
+          || otype == OP_CONST)
+
+/* Walk OP tree recursively, build ASTs, build subtrees */
+static pj_term_t *
+pj_build_ast(pTHX_ OP *o, ptrstack_t **subtrees, unsigned int *nvariables)
+{
+  const unsigned int parent_otype = o->op_type;
+  pj_term_t *retval = NULL;
+  OP *kid;
+
+  /* 2 is the maximum number of children that the supported
+   * OP types may have. Will change in future */
+  pj_term_t *kid_terms[2];
+  unsigned int ikid = 0;
+  unsigned int i;
+
+  PJ_DEBUG_1("pj_build_ast running on %s\n", OP_NAME(o));
+
+  if (o && (o->op_flags & OPf_KIDS)) {
+    for (kid = ((UNOP*)o)->op_first; kid; kid = kid->op_sibling) {
+      PJ_DEBUG_2("pj_build_ast considering kid (%u) type %s\n", ikid, OP_NAME(kid));
+
+      const unsigned int otype = kid->op_type;
+      if (otype == OP_CONST) {
+        kid_terms[ikid] = pj_make_const_dbl(SvNV(cSVOPx_sv(kid))); /* FIXME replace type by inferred type */
+      }
+      else if (otype == OP_PADSV) {
+        kid_terms[ikid] = pj_make_variable((*nvariables)++, pj_double_type); /* FIXME replace pj_double_type with type that's imposed by the current OP */
+        ptrstack_push(*subtrees, pj_double_type); /* FIXME replace pj_double_type with type that's imposed by the current OP */
+        ptrstack_push(*subtrees, o);
+      }
+      else if (IS_JITTABLE_OP_TYPE(otype)) {
+        kid_terms[ikid] = pj_build_ast(aTHX_ kid, subtrees, nvariables);
+        if (kid_terms[ikid] == NULL) {
+          for (i = 0; i < ikid; ++i)
+            pj_free_tree(kid_terms[ikid]);
+          return NULL;
+        }
+      }
+      else {
+        /* Can't represent OP with AST. So instead,
+         * recursively scan for separate candidates and
+         * treat as subtree. */
+        pj_find_jit_candidate(aTHX_ kid);
+        kid_terms[ikid] = pj_make_variable((*nvariables)++, pj_double_type); /* FIXME replace pj_double_type with type that's imposed by the current OP */
+
+        ptrstack_push(*subtrees, pj_double_type); /* FIXME replace pj_double_type with type that's imposed by the current OP */
+        ptrstack_push(*subtrees, kid);
+      }
+
+      ++ikid;
+    } /* end for kids */
+
+    /* FIXME find a way of doing this that is less manual/verbose */
+    if (parent_otype == OP_ADD) {
+      assert(ikid == 2);
+      retval = pj_make_binop(
+        pj_binop_add,
+        kid_terms[0],
+        kid_terms[1]
+      );
+    }
+    else if (parent_otype == OP_SUBTRACT) {
+      assert(ikid == 2);
+      retval = pj_make_binop(
+        pj_binop_subtract,
+        kid_terms[0],
+        kid_terms[1]
+      );
+    }
+    else {
+      PJ_DEBUG_1("Shouldn't happen! Unsupported OP!? %s", OP_NAME(o));
+      abort();
+    }
+
+  } /* end if has kids */
+  else { /* OP without kids */
+    /* OP_PADSV and OP_CONST are handled in the caller as other OPs' kids */
+    PJ_DEBUG_1("ARG! Unsupported OP without kids, %s", OP_NAME(o));
+    abort();
+  }
+
+  /* PMOP doesn't matter for JIT right now */
+  /*
+    if (o && OP_CLASS(o) == OA_PMOP && o->op_type != OP_PUSHRE
+          && (kid = PMOP_pmreplroot(cPMOPo)))
+    {}
+  */
+  return retval;
+}
 
 /* Starting from a candidate for JITing, walk the OP tree to accumulate
  * a subtree that can be replaced with a single JIT OP. */
@@ -20,10 +118,27 @@
 static void
 pj_attempt_jit(pTHX_ OP *o)
 {
-  PJ_DEBUG_1("Attempting JIT on %s\n", OP_NAME(o));
-}
+  /* In reality, we don't use the ptrstack_t as a proper stack,
+   * but more of a dynamically growing array */
+  ptrstack_t *subtrees;
+  pj_term_t *ast;
+  unsigned int nvariables = 0;
 
-#define IS_JITTABLE_OP_TYPE(otype) (otype == OP_ADD)
+  PJ_DEBUG_1("Attempting JIT on %s\n", OP_NAME(o));
+  subtrees = ptrstack_make(3, 0);
+
+  ast = pj_build_ast(aTHX_ o, &subtrees, &nvariables);
+
+  if (ast != NULL) {
+    /* TODO do stuff */
+    PJ_DEBUG("Built actual AST for jitting.\n");
+    if (DEBUGGING)
+      pj_dump_tree(ast);
+  }
+
+  pj_free_tree(ast);
+  ptrstack_free(subtrees);
+}
 
 /* inspired by B.xs */
 #define PMOP_pmreplstart(o)	o->op_pmstashstartu.op_pmreplstart
@@ -35,7 +150,7 @@ pj_attempt_jit(pTHX_ OP *o)
 void
 pj_find_jit_candidate(pTHX_ OP *o)
 {
-  const unsigned int otype = o->op_type;
+  unsigned int otype;
   OP *kid;
   ptrstack_t *backlog;
 
@@ -45,24 +160,28 @@ pj_find_jit_candidate(pTHX_ OP *o)
   /* Iterative tree traversal using stack */
   while (!ptrstack_empty(backlog)) {
     o = ptrstack_pop(backlog);
+    otype = o->op_type;
 
     PJ_DEBUG_1("Considering %s\n", OP_NAME(o));
 
-    if (IS_JITTABLE_OP_TYPE(otype))
+    /* Attempt JIT if the right OP type. Don't recurse if so. */
+    if (IS_JITTABLE_ROOT_OP_TYPE(otype)) {
       pj_attempt_jit(aTHX_ o);
+    }
+    else {
+      if (o && (o->op_flags & OPf_KIDS)) {
+        for (kid = ((UNOP*)o)->op_first; kid; kid = kid->op_sibling) {
+          ptrstack_push(backlog, kid);
+        }
+      }
 
-    if (o && (o->op_flags & OPf_KIDS)) {
-      for (kid = ((UNOP*)o)->op_first; kid; kid = kid->op_sibling) {
+      if (o && OP_CLASS(o) == OA_PMOP && o->op_type != OP_PUSHRE
+            && (kid = PMOP_pmreplroot(cPMOPo)))
+      {
         ptrstack_push(backlog, kid);
       }
-    }
-
-    if (o && OP_CLASS(o) == OA_PMOP && o->op_type != OP_PUSHRE
-          && (kid = PMOP_pmreplroot(cPMOPo)))
-    {
-      ptrstack_push(backlog, kid);
-    }
-  }
+    } /* end "not a jittable root OP" */
+  } /* end while stuff on todo stack */
 
   ptrstack_free(backlog);
 }
