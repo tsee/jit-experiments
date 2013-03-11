a51ebd32671c0b52ed20e9972b11b045b75c0edc
Steffen Mueller <smueller@cpan.org>
2013-03-11 18:53:41 +0100

    Add the most basic of tests written in C

--- Perl-JIT/ctest/100c_ast.c
+++ Perl-JIT/ctest/100c_ast.c
@@ -11,110 +11,81 @@
 
 #include "mytap.h"
 
+void basic_term_tests();
+
 int
 main ()
 {
-  ok_m(1, "alive");
+  basic_term_tests();
+
+  ok_m(1, "alive at end");
   done_testing();
   return 0;
 }
 
-int
-old_main(int argc, char **argv)
+#define TEST_ARGS_MAX 20
+
+void
+basic_term_tests()
 {
-  pj_term_t *t;
+  const unsigned int ntests = 2;
+  pj_term_t *test_tree[ntests];
+  unsigned int test_inputcount[ntests];
+  double *test_input[ntests];
+  double test_output[ntests];
+  char *test_name[ntests];
+  unsigned int i;
 
-  /* initialize tree structure */
 
-  t = pj_make_binop(
-    pj_binop_add,
-    pj_make_unop(
-      pj_unop_perl_int,
-      pj_make_variable(0, pj_double_type)
-    ),
-    pj_make_variable(1, pj_double_type)
+  i = 0;
+  test_name[i] = "negate float variable";
+  test_inputcount[i] = 1;
+  test_tree[i] = pj_make_unop(
+    pj_unop_negate,
+    pj_make_variable(0, pj_double_type)
   );
+  test_input[i] = (double *)malloc(sizeof(double)*1);
+  test_input[i][0] = 3.1;
+  test_output[i] = -3.1;
 
-  /* Just some debug output for the tree */
-  pj_dump_tree(t);
+  i = 1;
+  test_name[i] = "multiply float variable with constant";
+  test_inputcount[i] = 1;
+  test_tree[i] = pj_make_binop(
+    pj_binop_multiply,
+    pj_make_variable(0, pj_double_type),
+    pj_make_const_dbl(2.1)
+  );
+  test_input[i] = (double *)malloc(sizeof(double)*1);
+  test_input[i][0] = -5;
+  test_output[i] = (-5) * 2.1;
 
-  /* Setup JIT compiler */
-  jit_context_t context;
-  jit_function_t func = NULL;
 
-  context = jit_context_create();
 
-  /* Compile tree to function */
-  pj_basic_type funtype;
-  if (0 == pj_tree_jit(context, t, &func, &funtype)) {
-    printf("JIT succeeded!\n");
-  } else {
-    printf("JIT failed!\n");
+  for (i = 0; i < ntests; ++i) {
+    jit_context_t context;
+    pj_basic_type funtype;
+    jit_function_t func = NULL;
+    void *closure;
+    double result = 0.;
+    char namebuf[1024];
+
+    context = jit_context_create();
+
+    /* Compile tree to function */
+    sprintf(namebuf, "%s, JIT succeeded", test_name[i]);
+    ok_m(0 == pj_tree_jit(context, test_tree[i], &func, &funtype), namebuf);
+
+    closure = jit_function_to_closure(func);
+    pj_invoke_func((pj_invoke_func_t)closure, test_input[i], test_inputcount[i], funtype, (void *)&result);
+    sprintf(namebuf, "%s, result correct", test_name[i]);
+    is_double_m(1e-9, result, test_output[i], namebuf);
+
+    jit_context_destroy(context);
+
+    pj_free_tree(test_tree[i]);
+    free(test_input[i]);
   }
 
-  /* Setup function args */
-  double arg1, arg2;
-  void *args[2];
-  double result;
-  arg1 = -2.8;
-  arg1 = 3.5;
-  args[0] = &arg1;
-  args[1] = &arg2;
-
-  /* Call function (inefficiently) */
-  jit_function_apply(func, args, &result);
-  printf("foo(%f, %f) = %f\n", (float)arg1, (float)arg2, (float)result);
-
-  void *cl = jit_function_to_closure(func);
-  double (*fptr)(double, double) = cl;
-  result = fptr(arg1, arg2);
-  printf("foo(%f, %f) = %f\n", (float)arg1, (float)arg2, (float)result);
-
-  /* format of the args data structure on pj_invoke_func is different from
-   * jit_function_apply because we only support one argument type for now
-   * anyway. */
-  double args2[2];
-  args2[0] = arg1;
-  args2[1] = arg2;
-  //int i;
-  //for (i=0;i<1e8;++i){
-  pj_invoke_func((pj_invoke_func_t)fptr, &args2, 2, funtype, (void *)&result);
-  //}
-  printf("foo(%f, %f) = %f\n", (float)arg1, (float)arg2, (float)result);
-
-  /* Call function again, with slightly different input */
-  arg2 = 3.8;
-  jit_function_apply(func, args, &result);
-  printf("foo(%f, %f) = %f\n", (float)arg1, (float)arg2, (float)result);
-
-  /* cleanup */
-  pj_free_tree(t);
-
-  /* another example */
- 
-  t =  /*   pj_make_unop(
-      pj_unop_not_bool,*/
-      pj_make_binop(
-      pj_binop_ne,
-      pj_make_variable(0, pj_double_type),
-      pj_make_const_dbl(0)
-        )
-	/*)*/
-	;
-  
-  if (0 == pj_tree_jit(context, t, &func, &funtype)) {
-    printf("JIT succeeded again!\n");
-  } else {
-    printf("JIT failed!\n");
-  }
-  pj_dump_tree(t);
-
-  arg1 = 1.;
-  arg2 = 0.;
-  jit_function_apply(func, args, &result);
-  printf("foo(%f, %f) = %f\n", (float)arg1, (float)arg2, (float)result);
-
-  jit_context_destroy(context);
-  return 0;
 }
 
