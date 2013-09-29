dae7d3f83814fc524f388f695c047852a9c443a1
Mattia Barbon <mattia@barbon.org>
2013-09-29 13:11:01 +0200

    Fix unused value warnings for disabled PJ_DEBUG* macros

--- Perl-JIT/src/pj_debug.h
+++ Perl-JIT/src/pj_debug.h
@@ -5,9 +5,9 @@
 
 #ifdef NDEBUG
 #  define PJ_DEBUGGING 0
-#  define PJ_DEBUG (void)
-#  define PJ_DEBUG_1 (void)
-#  define PJ_DEBUG_2 (void)
+#  define PJ_DEBUG(s) ((void)(s))
+#  define PJ_DEBUG_1(s, par1) ((void)(s), (void)(par1))
+#  define PJ_DEBUG_2(s, par1, par2) ((void)(s), (void)(par1), (void)(par2))
 #else
 #  define PJ_DEBUGGING 1
 #  define PJ_DEBUG(s) do { printf(s); fflush(stdout); } while(0)
